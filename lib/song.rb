ab25a1f821c5a6b8dd5050ad24ea1c0b1b381b1b
Sarah H <sarahrose.henderson@gmail.com>
2019-09-20 00:10:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,16 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+  end
+
+  def self.create(name)
+    @@all << self
+    name
+  end
+
   def self.all
     @@all
   end
@@ -9,5 +19,7 @@ class Song
   def save
     self.class.all << self
   end
+
+
 
 end
